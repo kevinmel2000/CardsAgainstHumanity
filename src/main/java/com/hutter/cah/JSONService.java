9c1e9e4d25dd70bd771a3d3b46cb15b6464d8208
Scott Hutter <scott.hutter@gmail.com>
2017-08-18 00:05:04 -0500

    Updated to maven project

---
+++ src/main/java/com/hutter/cah/JSONService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hutter.cah;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author scott
+ */
+@Path("/status/")
+public class JSONService {
+
+	@GET
+	//@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Track getTrackInJSON() {
+
+		Track track = new Track();
+		track.setTitle("Enter Sandman");
+		track.setSinger("Metallica");
+
+		return track;
+
+	}
+
+	@POST
+	//@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Track track) {
+
+		String result = "Track saved : " + track;
+		return Response.status(201).entity(result).build();
+
+	}
+}
